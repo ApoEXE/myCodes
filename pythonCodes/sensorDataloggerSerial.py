7b21d252ad1cb27b4962baea8989d41deeb724f6
Javier Pernia <javierpernia89@gmail.com>
2020-03-18 20:32:34 -0500

    working version for unknown sensor monitoring

--- pythonCodes/sensorDataloggerSerial.py
+++ pythonCodes/sensorDataloggerSerial.py
@@ -6,8 +6,8 @@ import threading
 import csv
 from os import path as fileExist
 
-#		ID date time temp pres vol timeDelta
-all_IDs = [[],[],[],[],[],[],[]]
+#		ID date time temp pres vol timeDelta on/off
+all_IDs = [[],[],[],[],[],[],[],[]]
 dev_path = ''
 
 def timeDiff(newtime, oldtime):
@@ -62,6 +62,7 @@ def extract(msg):
 	  	VCODE = "0x"+tmp[17:19]
 	  	PM	= 	"0x"+tmp[20:22]
 	  	PL	=	"0x"+tmp[23:25]
+	  	SW  = tmp[33:35]
 	  	tHex = int(TCODE,16)
 	  	vHex = int(VCODE,16)
 	  	pmHex = int(PM,16)
@@ -73,34 +74,64 @@ def extract(msg):
 	  	volt = round(voltage(vHex),2)
 	  	temp = round(temperature(tHex),2)
 	  	pres = round(pressure(pmHex,plHex),2)
-	  	all_IDs[0].append(ID)
+
+	  	size,index = lookup_lastIndex(ID)
 	  	now = datetime.now()
 	  	now=now.strftime("%y-%m-%d %H:%M:%S")
 	  	date,time = str(now).split()
+
+	  	all_IDs[0].append(ID)
 	  	all_IDs[1].append(date)
 	  	all_IDs[2].append(time)
 	  	all_IDs[3].append(temp)
 	  	all_IDs[4].append(pres)
 	  	all_IDs[5].append(volt)
+	  	STATUS =''
+	  	if SW == '01' or SW == '00':
+	  		STATUS='ON'
+	  		all_IDs[7].append('ON')
+	  	else:
+	  		STATUS='OFF'
+	  		all_IDs[7].append('OFF')
 	  	#print(len(all_IDs[0]))
 	  	minutes=0.
-	  	if(len(all_IDs[0])==1):
+
+	  	if size < 1:
 	  		all_IDs[6].append(minutes)
 	  		saveCSV()
-	  		print("ID: ", ID, "T: ", temp, "V: ", volt, "P: ", pres, "DELTA: ", minutes)
-	  	elif(len(all_IDs[0])>1):
-	  		minutes=timeDiff(time,all_IDs[2][-2])
+	  		print(date,"",time,"ID: ", ID, "T: ", temp, "V: ", volt, "P: ", pres, "DELTA: ", minutes, "STATUS:",STATUS)
+	  	elif size >=2:
+	  		#print("ID:",ID,"Index:",index,"size:",size)
+	  		minutes=timeDiff(time,all_IDs[2][index])
+	  		minutes=round(minutes,2)
 	  		if(minutes>=1):	
 	  			all_IDs[6].append(minutes)
 	  			saveCSV()
-	  			print("ID: ", ID, "T: ", temp, "V: ", volt, "P: ", pres, "DELTA: ", minutes)
-	  		
+	  			print(date,"",time,"ID:", ID, "T:", temp, "V:", volt, "P:", pres, "DELTA:", minutes, "STATUS:",STATUS)
+
+
+def lookup_lastIndex(ID):
+	global all_IDs
+	indexList = []
+	index = 0
+	if len(all_IDs[0]) > 0:
+		for x in all_IDs[0]:
+			if(x==ID):
+				indexList.append(index)
+			index+=1
+		if len(indexList) == 0:
+			return 0,-1
+		else:
+			return len(indexList),indexList[-1]
+	else:
+		return 0,-1
+	
 
 def saveCSV():
   global all_IDs
   with open(str(all_IDs[0][-1])+'.csv', 'a', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["ID", str(all_IDs[0][-1]),"DATE", str(all_IDs[1][-1]),"TIME", str(all_IDs[2][-1]),"TEMPERATURE", str(all_IDs[3][-1]),"PRESSURE", str(all_IDs[4][-1]),"VOLTAGE", str(all_IDs[5][-1]),"deltaTime:", str(all_IDs[6][-1])])
+    writer.writerow(["ID", str(all_IDs[0][-1]),"DATE", str(all_IDs[1][-1]),"TIME", str(all_IDs[2][-1]),"TEMPERATURE", str(all_IDs[3][-1]),"PRESSURE", str(all_IDs[4][-1]),"VOLTAGE", str(all_IDs[5][-1]),"deltaTime:", str(all_IDs[6][-1]),"STATUS", str(all_IDs[7][-1])])
 
 def serialRead():
     ser = serial.Serial(dev_path, timeout=None, baudrate=9600, xonxoff=False, rtscts=False, dsrdtr=False)
@@ -133,7 +164,6 @@ def setEnv():
 
 if __name__ == '__main__':
 	value = setEnv()
-	print(value)
 	if(value):
 		serialReading = threading.Thread(target=serialRead)
 		serialReading.start()
